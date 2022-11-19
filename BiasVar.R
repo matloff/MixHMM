879cca483f51621565343d10a3d41d2aa2945727
Norm Matloff <nsmatloff@ucdavis.edu>
2022-11-19 08:52:05 -0800

    overfit ex

--- BiasVar.R
+++ BiasVar.R
@@ -8,16 +8,20 @@ biasVar <- function(data,yName,deg)
    yCol <- which(names(data) == yName)
    cols <- c(setdiff(1:ncol(data),yCol),yCol)
    data <- data[,cols]
-   set.seed(9999)
-   pfout <- polyFit(data,deg)
+   tstRows <- sample(1:nrow(data),1000)
+   trn <- data[-tstRows,]
+   tst <- data[tstRows,]
+   pfout <- polyFit(trn,deg)
    vcv <- vcov(pfout$fit)
-   x1 <- data[1,-ncol(data)]
+   x1 <- trn[1,-ncol(data)]
    x1 <- as.matrix(x1)
    x1 <- getPoly(x1,deg)$xdata
    x1 <- matrix(x1[1,],nrow=1)
    x1 <- as.numeric(x1)
    x1 <- c(1,x1)
    x1 <- matrix(x1,nrow=1)
-   x1 %*% vcv %*% t(x1)
-
+   x1var <- x1 %*% vcv %*% t(x1)
+   preds <- predict(pfout,tst[,-ncol(tst)])
+   testAcc <- mean(abs(tst[,ncol(tst)] - preds))
+   c(x1var,testAcc)
 }
